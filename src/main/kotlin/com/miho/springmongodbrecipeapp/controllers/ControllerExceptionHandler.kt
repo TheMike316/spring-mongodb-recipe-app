8760a093ae5647bedad93388a47de614fd9cbf3e
Michael Hornegger <michael.hornegger@karriere.at>
2018-07-06 16:18:03 +0200

    WIP: use reactive thymeleaf

--- src/main/kotlin/com/miho/springmongodbrecipeapp/controllers/ControllerExceptionHandler.kt
+++ src/main/kotlin/com/miho/springmongodbrecipeapp/controllers/ControllerExceptionHandler.kt
@@ -6,12 +6,13 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.thymeleaf.exceptions.TemplateInputException
 
 @ControllerAdvice
 class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException::class)
+    @ExceptionHandler(NotFoundException::class, TemplateInputException::class)
     fun handleNotFoundException(e: Exception, model: Model): String {
 
         model.addAttribute("exception", e)
